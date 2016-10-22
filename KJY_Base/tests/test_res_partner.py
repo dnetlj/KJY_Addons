cc945f92d52145dbe37e1e6219de3fa04be45108
彭宇波 <pengyb@gmail.com>
2016-10-22 15:22:16 +0800

    修改供应商信用额度的测试方法

--- KJY_Base/tests/test_res_partner.py
+++ KJY_Base/tests/test_res_partner.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from odoo.tests.common import HttpCase
-from odoo.exceptions import ValidationError
+from odoo.exceptions import AccessError
+from odoo.tests.common import TransactionCase
 
-class ResPartnerTestCase(HttpCase):
+
+class TestRules(TransactionCase):
     """ This class extends the base TransactionCase, in order to test the
     accounting with localization setups. It is configured to run the tests after
     the installation of all modules, and will SKIP TESTS ifit  cannot find an already
@@ -13,58 +14,28 @@ class ResPartnerTestCase(HttpCase):
     at_install = False
 
     def setUp(self):
-        pass
+        super(TestRules, self).setUp()
 
-    def test_supplier_invoice(self):
-        supplier = self.env['res.partner'].create({
+        self.supplier = self.env['res.partner'].create({
             'name': 'Test Supplier',
             'is_supplier': True,
             'supplier_credit_limit': 1000000,
-            'supplier_credit_limit_available': 1000000,
+            'supplier_credit_limit_available': 1000000
         })
 
+    def test_supplier_credit(self):
+        '''
+        供应商的信用额度的测试
+        :return:
+        '''
         # 扣除额度测试
-        supplier.deduction_supplier_credit_available(20000)
-        self.assertTrue(supplier.supplier_credit_limit_available == 980000)
+        self.supplier.deduction_supplier_credit_available(20000)
+        self.assertEqual(self.supplier.supplier_credit_limit_available, 980000)
 
         # 恢复额度测试
-        supplier.revert_supplier_credit_available(10000)
-        self.assertTrue(supplier.supplier_credit_limit_available == 990000)
+        self.supplier.revert_supplier_credit_available(10000)
+        self.assertEqual(self.supplier.supplier_credit_limit_available, 990000)
 
         # 恢复额度测试（超额度限制）
-        supplier.revert_supplier_credit_available(20000)
-        self.assertTrue(supplier.supplier_credit_limit_available == 1000000)
-
-
-        analytic_account = self.env['account.analytic.account'].create({
-            'name': 'test account',
-        })
-
-        # Should be changed by automatic on_change later
-        invoice_account = self.env['account.account'].search([('user_type_id', '=', self.env.ref('account.data_account_type_receivable').id)], limit=1).id
-        invoice_line_account = self.env['account.account'].search([('user_type_id', '=', self.env.ref('account.data_account_type_expenses').id)], limit=1).id
-
-        invoice = self.env['account.invoice'].create({'partner_id': self.env.ref('base.res_partner_2').id,
-            'account_id': invoice_account,
-            'type': 'in_invoice',
-        })
-
-        self.env['account.invoice.line'].create({'product_id': self.env.ref('product.product_product_4').id,
-            'quantity': 1.0,
-            'price_unit': 100.0,
-            'invoice_id': invoice.id,
-            'name': 'product that cost 100',
-            'account_id': invoice_line_account,
-            'invoice_line_tax_ids': [(6, 0, [tax.id])],
-            'account_analytic_id': analytic_account.id,
-        })
-
-        # check that Initially supplier bill state is "Draft"
-        self.assertTrue((invoice.state == 'draft'), "Initially vendor bill state is Draft")
-
-        #change the state of invoice to open by clicking Validate button
-        invoice.action_invoice_open()
-
-        #I cancel the account move which is in posted state and verifies that it gives warning message
-        with self.assertRaises(Warning):
-            invoice.move_id.button_cancel()
+        self.supplier.revert_supplier_credit_available(20000)
+        self.assertEqual(self.supplier.supplier_credit_limit_available, 1000000)
