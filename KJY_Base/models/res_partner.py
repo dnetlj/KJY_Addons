7547e6a8cd5c94f246ce2c90c55c29718bcd8362
彭宇波 <pengyb@gmail.com>
2016-10-22 16:05:22 +0800

    fix supplier credit limit bug

--- KJY_Base/models/res_partner.py
+++ KJY_Base/models/res_partner.py
@@ -13,22 +13,21 @@ class ResPartner(models.Model):
     p_card_no = fields.Char(string=u'身份证编号')
     p_card_img = fields.Binary(string=u'身份证图片')
 
-    client_level = fields.Selection([('A','A'),('B','B'),('C','C'),('D','D')],string=u'客户等级')
+    client_level = fields.Selection([('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D')], string=u'客户等级')
 
-    supplier_credit_limit = fields.Float(string=u'供应商信用额度',default=0.0)
+    supplier_credit_limit = fields.Float(string=u'供应商信用额度', default=0.0)
     supplier_credit_limit_available = fields.Float(string=u'供应商信用额度', default=0.0)
 
     @api.multi
-    def revert_supplier_credit_available(self,credit_limit,related_no=None,related_type=None):
-        '''
+    def revert_supplier_credit_available(self, credit_limit, related_no=None, related_type=None):
+        """
         恢复供应商可用信用额度
         :param credit_limit:
         :param related_no:
         :param related_type:
         :return:
-        '''
-        self
-        if credit_limit > self.supplier_credit_limit_available:
+        """
+        if credit_limit > 0:
             self.supplier_credit_limit_available += credit_limit
             if self.supplier_credit_limit_available > self.supplier_credit_limit:
                 self.supplier_credit_limit_available = self.supplier_credit_limit
@@ -36,16 +35,15 @@ class ResPartner(models.Model):
 
     @api.multi
     def deduction_supplier_credit_available(self, credit_limit, related_no=None, related_type=None):
-        '''
+        """
         扣除供应商可用信用额度
         :param credit_limit:
         :param related_no:
         :param related_type:
         :return:
-        '''
-        if credit_limit <= self.supplier_credit_limit_available:
+        """
+        if credit_limit > 0 and credit_limit <= self.supplier_credit_limit_available:
             self.supplier_credit_limit_available -= credit_limit
             return True
         else:
             raise Warning(_(u'该供应商信用额度不足,目前可用额度: %s' % self.supplier_credit_limit_available))
-
